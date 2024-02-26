998ecbe9691f5cd1ff6d563ccc83d429c8290600
Ricardo Ribeiro Leitinho <ricardo.leitinho@ebserh.gov.br>
2024-02-26 10:34:01 -0300

    Removed auto install from treesitter

--- lua/ohnitiel/plugins/treesitter.lua
+++ lua/ohnitiel/plugins/treesitter.lua
@@ -1,18 +1,19 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		
-		configs.setup({
-			ensure_installed = {
-				"lua", "typescript", "html", "bash", "css",
-				"json", "php", "python", "scss", "sql", "toml",
-				"javascript", "yaml", "dockerfile",
-			},
-			sync_installed = false,
-			highlight = { enable = true },
-			indent = {enable = true },
-		})
-	end
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "lua", "typescript", "html", "bash", "css",
+                "json", "php", "python", "scss", "sql", "toml",
+                "javascript", "yaml", "dockerfile",
+            },
+            sync_installed = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+            auto_install = false,
+        })
+    end
 }
