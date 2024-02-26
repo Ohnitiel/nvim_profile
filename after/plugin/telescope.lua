a58a5fa4cb883568901c5681da07d441966d15cc
Ricardo Ribeiro Leitinho <ricardo.leitinho@ebserh.gov.br>
2024-02-26 14:52:58 -0300

    Organized mapping. Added a few more LSP and git related mappings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,22 @@
 local telescope = require("telescope.builtin")
+
+-- Files maps
 vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
 vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
-vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
-vim.keymap.set("n", "<leader>gs", telescope.git_files, {})
-vim.keymap.set("n", "<leader>sh", telescope.search_history, {})
+
+-- Git maps
+vim.keymap.set("n", "<leader>gf", telescope.git_files, {})
+vim.keymap.set("n", "<leader>gc", telescope.git_commits, {})
+vim.keymap.set("n", "<leader>gs", telescope.git_stash, {})
+
+-- LSP maps
+vim.keymap.set("n", "gr", function() telescope.lps_references() end)
+vim.keymap.set("n", "gi", function() telescope.lps_implementations() end)
+vim.keymap.set("n", "<leader>qf", telescope.quickfix, {})
+vim.keymap.set("n", "<leader>D", telescope.diagnostics, {})
+vim.keymap.set("n", "<leader>d", function()
+    telescope.diagnostics({
+        bufnr = 0
+    })
+end)
