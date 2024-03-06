91264d89123f99f254db3c3a55f0dafc77122cd9
Ricardo Ribeiro Leitinho <ricardo.leitinho@ebserh.gov.br>
2024-03-06 14:35:57 -0300

    Started using mason-lsp handlers

--- lua/ohnitiel/plugins/lspconfig.lua
+++ lua/ohnitiel/plugins/lspconfig.lua
@@ -9,49 +9,65 @@ return {
     config = function()
         require("neodev").setup({})
         require("mason").setup()
+
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local lspconfig = require("lspconfig")
+        local handlers = {
+            function(server_name) -- default handler (optional)
+                lspconfig[server_name].setup({})
+            end,
+
+            ["lua_ls"] = function()
+                lspconfig.lua_ls.setup({
+                    settings = {
+                        Lua = {
+                            completion = { callSnippet = "Replace" }
+                        }
+                    },
+                    capabilities = capabilities
+                })
+            end,
+
+            ["intelephense"] = function()
+                lspconfig.intelephense.setup({
+                    capabilities = capabilities
+                })
+            end,
+
+            ["pyright"] = function()
+                lspconfig.pyright.setup({
+                    capabilities = capabilities
+                })
+            end,
+        }
+
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "pyright",
                 "lua_ls",
                 "intelephense",
             },
-        })
-        local lspconfig = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        lspconfig.lua_ls.setup({
-            settings = {
-                Lua = {
-                    completion = { callSnippet = "Replace" }
-                }
-            },
-            capabilities = capabilities
+            handlers = handlers,
         })
 
-        lspconfig.intelephense.setup({
-            capabilities = capabilities
-        })
-
-        lspconfig.pyright.setup({
-            capabilities = capabilities
-        })
 
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
                 vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "LSP go to declaration",})
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "LSP go to definition",})
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "LSP hover",})
-                vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { buffer = ev.buf, desc = "LSP signature help",})
-                vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = ev.buf, desc = "LSP rename",})
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "LSP go to declaration", })
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "LSP go to definition", })
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "LSP hover", })
+                vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help,
+                    { buffer = ev.buf, desc = "LSP signature help", })
+                vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = ev.buf, desc = "LSP rename", })
                 vim.keymap.set("n", "<F3>", function()
                     vim.lsp.buf.format({ async = true })
-                end, { buffer = ev.buf, desc = "LSP format file",})
+                end, { buffer = ev.buf, desc = "LSP format file", })
                 vim.keymap.set("n", "<F4>", function()
                     vim.lsp.buf.code_action({ apply = true })
-                end, { buffer = ev.buf, desc = "LSP code action",})
+                end, { buffer = ev.buf, desc = "LSP code action", })
             end
         })
     end
