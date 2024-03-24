68a28a73ca066f03c49ea83c8c85c5cec2fa500b
= <ricardo.leitinho@ebserh.gov.br>
2024-03-24 11:27:25 -0300

    Added dadbod with ui and completion

--- lua/ohnitiel/plugins/init.lua
+++ lua/ohnitiel/plugins/init.lua
@@ -1,6 +1,16 @@
 return {
 
     {
+        "tpope/vim-dadbod",
+        dependencies = {
+            "kristijanhusak/vim-dadbod-completion",
+            "kristijanhusak/vim-dadbod-ui"
+        },
+        config = function()
+            require("config.dadbod").setup()
+        end,
+    },
+    {
         "neogitOrg/neogit",
         dependencies = {
             "nvim-lua/plenary.nvim",
