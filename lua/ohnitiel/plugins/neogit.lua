795cf17f4df89354fd6518b75774697c9ddd4ed8
= <ricardo.leitinho@ebserh.gov.br>
2024-03-24 11:48:19 -0300

    Moved neogit to it's own file. Setup a simple keymap.

---
+++ lua/ohnitiel/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+    "neogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+        local ng = require("neogit")
+
+        vim.keymap.set("n", "<C-g>", function() ng.open() end, { desc = "Open Git" })
+    end,
+
+}
