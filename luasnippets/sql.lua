07277db7afc1c61cdeee6514ee12cb96c9333ebf
= <ricardo.leitinho@ebserh.gov.br>
2024-03-30 09:24:03 -0300

    Added new snippet

--- luasnippets/sql.lua
+++ luasnippets/sql.lua
@@ -12,7 +12,7 @@ local rep = require("luasnip.extras").rep
 ls.add_snippets("sql", {
 
     s("CREATE TABLE", {
-        t("CREATE TABLE "), i(1, "table_name"), t({ " (",
+        t("CREATE TABLE IF NOT EXISTS "), i(1, "table_name"), t({ " (",
         "\tid SERIAL PRIMARY KEY,",
         "\tcreated_at TIMESTAMP DEFAULT NOW(),",
         "\tupdated_at TIMESTAMP DEFAULT NOW(),",
@@ -20,31 +20,40 @@ ls.add_snippets("sql", {
         ")" }),
     }),
 
-    s("RLT TABLE", { sn(1, {
-        t("CREATE TABLE "), i(1, "schema"), t(".rlt_"), i(2, "table1"), t("_"), i(3, "table2"), t({ " (",
-        "\tid SERIAL PRIMARY KEY,", "" }),
-        f(function(args)
-            return {
-                "\t" ..
-                args[1][1] ..
-                "." ..
-                args[2][1] ..
-                "_id NOT NULL INTEGER REFERENCES " ..
-                args[1][1] .. "." .. args[2][1] .. "(id) ON UPDATE CASCADE ON DELETE CASCADE,",
-                "\t" ..
-                args[1][1] ..
-                "." ..
-                args[3][1] ..
-                "_id NOT NULL INTEGER REFERENCES " ..
-                args[1][1] .. "." .. args[3][1] .. "(id) ON UPDATE CASCADE ON DELETE CASCADE,",
-                ""
-            }
-        end, { 1, 2, 3 }
-        ),
-        t({ "\tcreated_at TIMESTAMP DEFAULT NOW(),",
-            "\tupdated_at TIMESTAMP DEFAULT NOW(),",
-            "\tdeleted_at TIMESTAMP", ")",
+    s("RLT TABLE", {
+        sn(1, {
+            t("CREATE TABLE IF NOT EXISTS "), i(1, "schema"), t(".rlt_"), i(2, "table1"), t("_"), i(3, "table2"), t({
+            " (",
+            "\tid SERIAL PRIMARY KEY,", "" }),
+            f(function(args)
+                return {
+                    "\t" ..
+                    args[1][1] ..
+                    "." ..
+                    args[2][1] ..
+                    "_id NOT NULL INTEGER REFERENCES " ..
+                    args[1][1] .. "." .. args[2][1] .. "(id) ON UPDATE CASCADE ON DELETE CASCADE,",
+                    "\t" ..
+                    args[1][1] ..
+                    "." ..
+                    args[3][1] ..
+                    "_id NOT NULL INTEGER REFERENCES " ..
+                    args[1][1] .. "." .. args[3][1] .. "(id) ON UPDATE CASCADE ON DELETE CASCADE,",
+                    ""
+                }
+            end, { 1, 2, 3 }
+            ),
+            t({ "\tcreated_at TIMESTAMP DEFAULT NOW(),",
+                "\tupdated_at TIMESTAMP DEFAULT NOW(),",
+                "\tdeleted_at TIMESTAMP", ")",
+            }),
         }),
     }),
+
+    s("ADD COLUMN", {
+        sn(1, {
+            t("ALTER TABLE "), i(1, "table", ""), t("ADD COLUMN "),
+            i(2, "column_name"), t(" "), i(3, "data_type"),
+        }),
     }),
 })
