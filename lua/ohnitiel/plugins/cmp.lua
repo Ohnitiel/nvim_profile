9fab59cd3ab3262f7cf8570e20c232c1fd415740
= <ricardo.leitinho@ebserh.gov.br>
2024-04-19 21:24:31 -0300

    Reconfigured cmp

--- lua/ohnitiel/plugins/cmp.lua
+++ lua/ohnitiel/plugins/cmp.lua
@@ -5,16 +5,23 @@ return {
         "hrsh7th/cmp-cmdline",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
-        "L3MON4D3/LuaSnip",
+        {
+            "L3MON4D3/LuaSnip",
+            build = (function()
+                return 'make install_jsregexp'
+            end)
+        },
         "saadparwaiz1/cmp_luasnip",
     },
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        luasnip.config.setup()
 
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
+                    luasnip.lsp_expand(args.body)
                 end,
             },
             window = {
@@ -24,15 +31,26 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<F5>'] = cmp.mapping.complete(),
+                ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-l>'] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    end
+                end, { 'i', 's' }),
+                ['<C-h>'] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    end
+                end, { 'i', 's' }),
             }),
             sources = cmp.config.sources(
                 {
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
                     { name = 'codeium' },
+                    { name = 'path' },
                 },
                 { { name = 'buffer' }, }
             )
@@ -46,9 +64,9 @@ return {
         cmp.setup.cmdline(':', {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                { name = 'cmdline' }
+                { name = 'path' },
+                { name = 'cmdline' },
+                { name = 'codeium' },
             })
         })
     end
