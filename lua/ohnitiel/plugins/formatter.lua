9f2f12a4063cd2c96ce1721b2fc8e418f9b07f21
= <ricardo.leitinho@ebserh.gov.br>
2024-05-01 08:30:15 -0300

    Configured sql-formatter

--- lua/ohnitiel/plugins/formatter.lua
+++ lua/ohnitiel/plugins/formatter.lua
@@ -1,21 +1,28 @@
 return {
-    'stevearc/conform.nvim',
+    "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
             lua = { "stylua" },
             javascript = { "prettierd" },
             typescript = { "prettierd" },
-            sql = { "sqlfmt" },
+            sql = { "sql_formatter" },
             json = { "jq" },
             sh = { "beautysh" },
-        }
+        },
+        formatters = {
+            sql_formatter = {
+                args = { "-c", tostring(vim.fs.find("sql-formatter.json", { upward = true })[1]) }
+            },
+        },
     },
     keys = {
         {
-            '<F3>',
-            function() require('conform').format({ async = true, lsp_fallback = true }) end,
-            mode = '',
-            desc = 'Format buffer'
-        }
-    }
+            "<F3>",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
 }
