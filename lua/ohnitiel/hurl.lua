d51da9e115d0f9de0ff69419267a07a944e0a7d4
= <ricardo.leitinho@ebserh.gov.br>
2024-05-01 20:40:09 -0300

    Formatting

--- lua/ohnitiel/hurl.lua
+++ lua/ohnitiel/hurl.lua
@@ -2,7 +2,8 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*.hurl",
     callback = function()
         vim.cmd.setf("hurl")
-        vim.keymap.set({ "n", "v" }, "<F5>", ":HurlRunner<CR>", { desc = "Run selected request", buffer = true })
+        vim.keymap.set({ "n", "v" }, "<F5>", ":HurlRunner<CR>",
+            { desc = "Run selected request", buffer = true })
         vim.keymap.set({ "n", "v" }, "<leader>v", ":HurlRunnerVerbose<CR>",
             { desc = "Run selected request in verbose mode", buffer = true })
     end
