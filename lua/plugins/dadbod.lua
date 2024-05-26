76005e4fde40fd3b4932aa436924c836e7961e52
Ricardo Ribeiro Leitinho <ricardo.leitinho@ebserh.gov.br>
2024-05-26 09:59:20 -0300

    Finished setting up DBUI

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,7 +1,10 @@
 return {
-    "tpope/vim-dadbod",
-    "kristijanhusak/vim-dadbod-completion",
     "kristijanhusak/vim-dadbod-ui",
+
+    dependencies = {
+        "tpope/vim-dadbod",
+        "kristijanhusak/vim-dadbod-completion",
+    },
 
     config = function()
         vim.g.db_ui_table_helpers = {
@@ -10,22 +13,16 @@ return {
                 list = "SELECT * FROM {table} LIMIT 50",
             }
         }
+
         vim.g.db_ui_use_nerd_fonts = 1
 
         local json = require("functions.json")
-
 
         local db_file = io.input(os.getenv("HOME") .. "/.dotfiles/databases.json")
         local content = io.read("*a")
         local databases = json.from_string(content)
         io.close(db_file)
 
-        for _, v in pairs(databases) do
-            databases = v
-        end
-
-        print(databases[1]['name'])
-
-        _G.dbs = databases
+        vim.g.dbs = databases['databases']
     end,
 }
