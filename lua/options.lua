167c519602c9def9d07b539d12487f15b62b087d
Ohnitiel <ohnitiel@BRBNB007534.globalhitss.com.br>
2024-06-20 17:02:39 -0300

    Added clipboard with win32yank

--- lua/options.lua
+++ lua/options.lua
@@ -14,3 +14,19 @@ vim.opt.colorcolumn = "80"
 vim.opt.inccommand = "split"
 
 vim.opt.shada = { "'10", "<0", "s10", "h" }
+
+vim.cmd([[
+let g:clipboard = {
+\ 'name': 'WSL',
+\ 'copy': {
+\    '+':'win32yank.exe -i',
+\    '*':'win32yank.exe -i'
+\ },
+\   'paste': {
+\      '+': 'win32yank.exe -o',
+\      '*': 'win32yank.exe -o',
+\   },
+\   'cache_enabled': 0,
+\ }
+]]
+)
