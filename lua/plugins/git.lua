ce350ba8e118464b4c9b725c1c14d4269870c48a
= <=>
2024-10-15 10:38:34 -0300

    Added git keymaps and gitignore

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,5 +5,19 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
 
-    config = true
+    config = function()
+        local git = require("vgit")
+        git.setup()
+
+        vim.keymap.set("n", "<C-k>", git.hunk_up)
+        vim.keymap.set("n", "<C-j>", git.hunk_down)
+        vim.keymap.set("n", "<leader>gs", git.buffer_hunk_stage)
+        vim.keymap.set("n", "<leader>gS", git.buffer_stage)
+        vim.keymap.set("n", "<leader>gU", git.buffer_unstage)
+        vim.keymap.set("n", "<leader>gr", git.buffer_hunk_reset)
+        vim.keymap.set("n", "<leader>gR", git.buffer_reset)
+        vim.keymap.set("n", "<leader>gp", git.buffer_hunk_preview)
+        vim.keymap.set("n", "<leader>gd", git.buffer_diff_preview)
+        vim.keymap.set("n", "<C-g>", git.project_commit_preview)
+    end
 }
