1ae6a1b2cc056888776a208a2f8fb5a6ad1a290f
Ohnitiel <leitinho.ricardo@gmail.com>
2025-01-09 16:59:01 -0300

    Open split terminal on startup

--- lua/config/remap/terminal.lua
+++ lua/config/remap/terminal.lua
@@ -2,15 +2,19 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { noremap = true })
 
 local job_id = 0
 
-vim.keymap.set("n", "<leader>tt", function()
+local new_term = function()
     vim.cmd.vsplit()
     vim.cmd.wincmd("l")
     vim.cmd.terminal()
 
     job_id = vim.bo.channel
     vim.cmd.wincmd("j")
-end)
+end
+
+vim.keymap.set("n", "<leader>tt", new_term)
 
 vim.keymap.set("n", "<C-g>", function()
     vim.fn.chansend(job_id, { "git status\r\n" })
 end)
+
+new_term()
