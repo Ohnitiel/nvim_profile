ad5f9f7973478ff51da38f060304b955cb23a4f0
Ohnitiel <leitinho.ricardo@gmail.com>
2025-01-10 16:11:16 -0300

    Fix cmp on command line

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -107,8 +107,10 @@ return {
         })
 
         cmp.setup.cmdline({ ':' }, {
+            mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources(
-                { { name = 'path' } }, { { name = 'cmdline' } }
+                { { name = 'cmdline' } },
+                { { name = 'path' } }
             ),
             matching = {
                 disallow_symbol_nonprefix_matching = false,
