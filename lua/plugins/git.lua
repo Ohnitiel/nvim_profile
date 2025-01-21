7cd230e578d6685c7a9f7117b8eb2bcdf52fa7f8
Ohnitiel <leitinho.ricardo@gmail.com>
2025-01-21 18:36:25 -0300

    Added line blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,8 +5,10 @@ return {
         require("gitsigns").setup({
             current_line_blame = true,
             current_line_blame_opts = {
-                delay = 500
+                virt_text_pos = "right_align",
+                delay = 500,
             },
+            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
         })
     end
 }
