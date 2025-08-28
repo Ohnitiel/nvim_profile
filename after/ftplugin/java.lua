223509b63652cd9f8ff377c0568c2f52275ae3ca
Ohnitiel <leitinho.ricardo@gmail.com>
2025-08-28 10:15:09 -0300

    Changed adapter

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,19 +1,19 @@
 local dap = require("dap")
 
--- dap.adapters.java = function(callback)
---     callback({
---         type = 'server',
---         host = '127.0.0.1',
---         port = 8787
---     })
--- end
+dap.adapters.java = function(callback)
+    callback({
+        type = 'server',
+        host = '127.0.0.1',
+        port = 8787
+    })
+end
 
-dap.adapters.java = {
-    type = "executable",
-    command = "java",
-    args = { "-jar", vim.p.masonpath .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-0.53.2.jar"},
-
-}
+-- dap.adapters.java = {
+--     type = "executable",
+--     command = "java",
+--     args = { "-jar", vim.p.masonpath .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-0.53.2.jar"},
+--
+-- }
 
 dap.configurations.java = {
     type = "java",
