1616cc78f3d80c04e26abdafd6583a8b61566644
Ohnitiel <leitinho.ricardo@gmail.com>
2025-08-28 10:19:51 -0300

    Changed adapter

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -2,9 +2,9 @@ local dap = require("dap")
 
 dap.adapters.java = function(callback)
     callback({
-        type = 'server',
-        host = '127.0.0.1',
-        port = 8787
+        type = "server",
+        host = "127.0.0.1",
+        port = port,
     })
 end
 
@@ -16,9 +16,11 @@ end
 -- }
 
 dap.configurations.java = {
-    type = "java",
-    request = "attach",
-    name = "Debug (Attach)",
-    hostName = "127.0.0.1",
-    port = 8787,
+    {
+        type = "java",
+        request = "attach",
+        name = "Debug (Attach)",
+        hostName = "127.0.0.1",
+        port = 8787,
+    },
 }
