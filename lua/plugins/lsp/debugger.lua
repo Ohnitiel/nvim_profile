e7e642a749589f0831e0fe784a04667a8aa85f2a
Ohnitiel <leitinho.ricardo@gmail.com>
2025-08-29 16:26:45 -0300

    Fix syntax errors

--- lua/plugins/lsp/debugger.lua
+++ lua/plugins/lsp/debugger.lua
@@ -39,23 +39,23 @@ require("lazydev").setup({
 })
 
 local dap, dapui = require("dap"), require("dapui")
-dap.listener.before.attach.dapui_config = function()
+dap.listeners.before.attach.dapui_config = function()
     dapui.open()
 end
-dap.listener.before.launch.dapui_config = function()
+dap.listeners.before.launch.dapui_config = function()
     dapui.open()
 end
-dap.listener.before.event_terminated.dapui_config = function()
+dap.listeners.before.event_terminated.dapui_config = function()
     dapui.close()
 end
-dap.listener.before.event_exited.dapui_config = function()
+dap.listeners.before.event_exited.dapui_config = function()
     dapui.close()
 end
 
-vim.keymap.add("v", "<M-k>", function()
+vim.keymap.set("v", "<M-k>", function()
     require("dapui").eval()
 end)
 
-vim.keymap.add("n", "<M-f>", function()
+vim.keymap.set("n", "<M-f>", function()
     require("dapui").float_element()
 end)
