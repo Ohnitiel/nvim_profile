7bc1322d78d7fdcf128743a311bcc0aff019fc2f
Ohnitiel <leitinho.ricardo@gmail.com>
2025-09-24 10:15:11 -0300

    Fixed root_dir

--- lsp/ty.lua
+++ lsp/ty.lua
@@ -1,5 +1,11 @@
 return {
     cmd = { "ty", "server" },
     filetypes = { "python" },
-    root_dir = vim.fs.root(0, {"pyproject.toml", "requirements.txt"}),
+    -- root_dir = function()
+    --     local root = vim.fs.root(0, { "pyproject.toml", "requirements.txt" })
+    --     if root == "." then
+    --         return "./."
+    --     end
+    --     return root
+    -- end
 }
